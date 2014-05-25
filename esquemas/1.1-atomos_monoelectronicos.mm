5a3048493dbca053c8e263a1d04a5c55582c5e36
Xavi Aznar <xavi.aznar@mail.local>
2014-05-25 13:04:52 +0200

    Esquema de la resolución de la ec. de Schrodinger.

--- esquemas/1.1-atomos_monoelectronicos.mm
+++ esquemas/1.1-atomos_monoelectronicos.mm
@@ -1,34 +1,34 @@
 <map version="freeplane 1.2.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="&#xc1;tomos monoelectr&#xf3;nicos" LOCALIZED_STYLE_REF="default" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1401012638402" MAX_WIDTH="200" MIN_WIDTH="1"><hook NAME="MapStyle" zoom="1.018">
+<node TEXT="&#xc1;tomos monoelectr&#xf3;nicos" LOCALIZED_STYLE_REF="default" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1401013850068" MAX_WIDTH="200" MIN_WIDTH="1"><hook NAME="MapStyle" zoom="1.105">
 
 <map_styles>
 <stylenode LOCALIZED_TEXT="styles.root_node" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
-<edge STYLE="horizontal" WIDTH="1"/>
 <font SIZE="12"/>
+<edge STYLE="horizontal" WIDTH="1"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
-<edge STYLE="horizontal" WIDTH="1"/>
 <font SIZE="12"/>
+<edge STYLE="horizontal" WIDTH="1"/>
 <stylenode LOCALIZED_TEXT="default" MAX_WIDTH="150" MIN_WIDTH="150" COLOR="#000000" STYLE="as_parent">
 <font NAME="SansSerif" SIZE="12" BOLD="false" ITALIC="false"/>
 <edge STYLE="horizontal" WIDTH="1"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
-<edge STYLE="horizontal" WIDTH="1"/>
 <font SIZE="12"/>
+<edge STYLE="horizontal" WIDTH="1"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.note" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
-<edge STYLE="horizontal" WIDTH="1"/>
 <font SIZE="12"/>
+<edge STYLE="horizontal" WIDTH="1"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.floating" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
-<edge STYLE="horizontal" WIDTH="1"/>
 <font SIZE="12"/>
+<edge STYLE="horizontal" WIDTH="1"/>
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
-<edge STYLE="horizontal" WIDTH="1"/>
 <font SIZE="12"/>
+<edge STYLE="horizontal" WIDTH="1"/>
 <stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
 <font NAME="Liberation Sans" SIZE="12" BOLD="true"/>
 <edge STYLE="horizontal" WIDTH="1"/>
@@ -43,13 +43,13 @@
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.important" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
 <icon BUILTIN="yes"/>
-<edge STYLE="horizontal" WIDTH="1"/>
 <font SIZE="12"/>
+<edge STYLE="horizontal" WIDTH="1"/>
 </stylenode>
 </stylenode>
 <stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
-<edge STYLE="horizontal" WIDTH="1"/>
 <font SIZE="12"/>
+<edge STYLE="horizontal" WIDTH="1"/>
 <stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000" STYLE="as_parent" MAX_WIDTH="150" MIN_WIDTH="150">
 <font SIZE="12"/>
 <edge STYLE="horizontal" WIDTH="1"/>
@@ -74,7 +74,6 @@
 </stylenode>
 </map_styles>
 </hook>
-<hook NAME="AutomaticEdgeColor" COUNTER="7"/>
 <hook NAME="AlwaysUnfoldedNode"/>
 <node TEXT="Ecuaci&#xf3;n de Schr&#xf6;dinger" POSITION="left" ID="ID_1067734256" CREATED="1401008680320" MODIFIED="1401012638404" MAX_WIDTH="200" MIN_WIDTH="200">
 <hook EQUATION="\mathcal{H}\psi(r, \theta, \varphi ) = E \psi (r,\theta ,\varphi )" NAME="plugins/latex/LatexNodeHook.properties"/>
@@ -93,13 +92,18 @@
 <node TEXT="Separaci&#xf3;n de variables" ID="ID_484465431" CREATED="1401008905449" MODIFIED="1401012638412" MAX_WIDTH="200" MIN_WIDTH="150">
 <hook EQUATION="\psi(r, \theta, \varphi ) = R(r)\Theta(\theta)\Phi(\varphi )" NAME="plugins/latex/LatexNodeHook.properties"/>
 <node TEXT="Ecuaci&#xf3;n Radial" ID="ID_227328454" CREATED="1401009881813" MODIFIED="1401012638413" MAX_WIDTH="200" MIN_WIDTH="200">
-<hook EQUATION="\frac{d}{dR}\left(&#xa;r^2 \frac{dR}{dr}&#xa;\right)&#xa;+&#xa;\frac{2m}{\hbar ^2}r^2&#xa;\left(&#xa;E+V(r) - C \right)R = 0" NAME="plugins/latex/LatexNodeHook.properties"/>
-<node TEXT="Polinomios de Laguerre" ID="ID_1086755435" CREATED="1401012032864" MODIFIED="1401012638414" MAX_WIDTH="200" MIN_WIDTH="200"/>
+<hook EQUATION="\frac{d}{dR}\left(&#xa;r^2 \frac{dR}{dr}&#xa;\right)&#xa;+&#xa;\frac{2m}{\hbar ^2}r^2&#xa;\left(&#xa;E+V(r) - l(l+1)) \right)R = 0" NAME="plugins/latex/LatexNodeHook.properties"/>
+<node TEXT="Soluciones finitas y normalizadas" ID="ID_674813998" CREATED="1401014751845" MODIFIED="1401014983252">
+<hook EQUATION="R_{n,l} (r) =&#xa;\sqrt{&#xa;\frac{(n-l-1)!} {(n+l)! 2n}    &#xa;}&#xa;\left(&#xa;2\frac{Z}{an}&#xa;\right)^{l+\frac{3}{2}}&#xa;r^l&#xa;\exp\left(2 \frac{Z}{an} r \right)&#xa;L_{n-l-1}^{2l+1}&#xa;\left( 2 \frac{Z}{an} r \right)" NAME="plugins/latex/LatexNodeHook.properties"/>
 </node>
-<node TEXT="Ecuaci&#xf3;n angular" ID="ID_1231539367" CREATED="1401009903259" MODIFIED="1401012638414" MAX_WIDTH="200" MIN_WIDTH="200">
-<hook EQUATION="\frac{1}{\sin\theta } \frac{d}{d\theta} \left(\sin\theta \frac{d\Theta}{d\theta}\right) + \left( C - \frac{m^2}{\sin^2 \theta }\right)\Theta  = 0" NAME="plugins/latex/LatexNodeHook.properties"/>
-<node TEXT="Polinomios de Legendre" ID="ID_292767321" CREATED="1401011979285" MODIFIED="1401012638415" MAX_WIDTH="200" MIN_WIDTH="200"/>
+<node TEXT="Polinomios de Laguerre (n=1,2,...;l=0,1,...,n-1)" ID="ID_1086755435" CREATED="1401012032864" MODIFIED="1401015532282" MAX_WIDTH="200" MIN_WIDTH="200">
+<hook EQUATION="L^{2l+1}_{n-l-1}\left( 2\frac{Z}{an}r \right) =&#xa;\frac{1}{(n-l-1)!}&#xa;\left( 2\frac{Z}{an} r \right)^{-(2l+1)}&#xa;\exp\left( 2\frac{Z}{an} r \right)&#xa;\frac{d^{n-l-1}}{d\left( 2\frac{Z}{an}r \right)^{n-l-1}}&#xa;\left(&#xa;\left( 2\frac{Z}{an} r \right)^{n+l}&#xa;\exp\left( -2\frac{Z}{an}r \right)&#xa;\right)" NAME="plugins/latex/LatexNodeHook.properties"/>
 </node>
+</node>
+<node TEXT="Parte angular" ID="ID_413921274" CREATED="1401014430946" MODIFIED="1401014645942">
+<hook EQUATION="l=0,1,2,\ldots\quad ;\quad -l\leq m \leq l" NAME="plugins/latex/LatexNodeHook.properties"/>
+<node TEXT="Arm&#xf3;nicos esf&#xe9;ricos" ID="ID_1776588765" CREATED="1401014493536" MODIFIED="1401014565015">
+<hook EQUATION="Y_{l,m}(\theta, \varphi ) = \frac{1}{\sqrt{2\pi}} \Theta_{l,m}(\theta)e^{im\varphi}" NAME="plugins/latex/LatexNodeHook.properties"/>
 <node TEXT="Ecuaci&#xf3;n angular" ID="ID_1656154007" CREATED="1401009918421" MODIFIED="1401012638415" MAX_WIDTH="200" MIN_WIDTH="200">
 <hook EQUATION="\frac{d^2 \Phi }{d\varphi^2} + k \Phi = 0 " NAME="plugins/latex/LatexNodeHook.properties"/>
 <node TEXT="Si k &lt; 0, soluci&#xf3;n trivial" ID="ID_145678150" CREATED="1401011238162" MODIFIED="1401012638415" MAX_WIDTH="200" MIN_WIDTH="200">
@@ -124,6 +128,36 @@
 <node TEXT="Constituyen una base" ID="ID_1813948932" CREATED="1401011838986" MODIFIED="1401012638419" MAX_WIDTH="200" MIN_WIDTH="150"/>
 </node>
 </node>
+<node TEXT="Ecuaci&#xf3;n angular" ID="ID_1231539367" CREATED="1401009903259" MODIFIED="1401012638414" MAX_WIDTH="200" MIN_WIDTH="200">
+<hook EQUATION="\frac{1}{\sin\theta } \frac{d}{d\theta} \left(\sin\theta \frac{d\Theta}{d\theta}\right) + \left( C - \frac{m^2}{\sin^2 \theta }\right)\Theta  = 0" NAME="plugins/latex/LatexNodeHook.properties"/>
+<node TEXT="Polinomios de Legendre" ID="ID_292767321" CREATED="1401011979285" MODIFIED="1401012638415" MAX_WIDTH="200" MIN_WIDTH="200"/>
+<node TEXT="Cambio de variable" ID="ID_902711953" CREATED="1401013799029" MODIFIED="1401013833266">
+<hook EQUATION="\mu = \cos\theta " NAME="plugins/latex/LatexNodeHook.properties"/>
+<node TEXT="Ecuaci&#xf3;n hipergeom&#xe9;trica" ID="ID_1069616741" CREATED="1401013916702" MODIFIED="1401013932748">
+<node TEXT="S&#xf3;lo tiene soluci&#xf3;n si se dan las relaciones entre los par&#xe1;metros l y m" ID="ID_590953767" CREATED="1401013959682" MODIFIED="1401014136250">
+<node TEXT="" ID="ID_77390329" CREATED="1401014095246" MODIFIED="1401014155984">
+<hook EQUATION="C=l(l+1)\qquad l=0,1,2,\ldots" NAME="plugins/latex/LatexNodeHook.properties"/>
+<hook NAME="FirstGroupNode"/>
+</node>
+<node TEXT="" ID="ID_830479580" CREATED="1401014126078" MODIFIED="1401014142787">
+<hook EQUATION=" \qquad -l\leq m \leq l" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="Soluciones finitas" ID="ID_269430259" CREATED="1401014155980" MODIFIED="1401014210594">
+<hook NAME="SummaryNode"/>
+<hook EQUATION="\Theta_{l,m}(\mu) = \frac{(-1)^{l-m}}{2^l l!} &#xa;\left(&#xa; \frac{2l+1}{2}&#xa; \frac{(l+m)!}{(l-m)!}&#xa;\right)^{1/2}&#xa;(1-\mu^2)^{-m/2}&#xa;\frac{d^{l-m}}{d\mu ^{l-m}}&#xa;(1-\mu^2)^l" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="Energ&#xed;a total del &#xe1;tomo" ID="ID_52017894" CREATED="1401015578050" MODIFIED="1401015587406">
+<node TEXT="E&lt;0" ID="ID_58169995" CREATED="1401015589052" MODIFIED="1401015649785">
+<hook EQUATION="E=-\frac{1}{n^2} \frac{Z^2 e^4 m}{2(4\pi\epsilon_0 )^2 \hbar ^2}" NAME="plugins/latex/LatexNodeHook.properties"/>
+</node>
+<node TEXT="E cuantificada" ID="ID_26945829" CREATED="1401015596184" MODIFIED="1401015599629"/>
 </node>
 </node>
 <node TEXT="N&#xfa;cleo est&#xe1;tico" POSITION="right" ID="ID_1322890249" CREATED="1401008542918" MODIFIED="1401012638419" MAX_WIDTH="200" MIN_WIDTH="200">
